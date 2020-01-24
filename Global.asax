53467c6f685e99b3a2975204c24a50969ad24048
tk848615 <tk848615@ohio.edu>
2020-01-24 15:16:20 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tk848615_MIS4200.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tk848615_MIS4200.Startup))]
+namespace tk848615_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
