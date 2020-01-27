08846ee0b5148f49522b4db62c12c1e094663eb4
tk848615 <tk848615@ohio.edu>
2020-01-27 11:41:16 -0500

    c;lass 1/27

---
+++ Models/customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tk848615_MIS4200.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public DateTime customerSince { get; set; }
+
+    }
+}
